812ffce64f86bea80075a24ced6606fe024b5965
ganesh123456788 <yanagandlaganesh@gmail.com>
2024-10-10 15:08:16 +0530

    contact us

---
+++ smtp contactus/Models/PaymentDB.Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace smtp_contactus.Models
+{
+	public class PaymentDbContext : DbContext
+	{
+		public PaymentDbContext() : base("name=DefaultConnection") { }
+
+		public DbSet<Payment> Payments { get; set; } // Maps to your payments table
+	}
+}
