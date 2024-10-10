9ccf7d6b453f37345fef686c93d1abde7bb9f6e2
ganesh123456788 <yanagandlaganesh@gmail.com>
2024-10-10 19:55:53 +0530

    Revert "regestration"
    This reverts commit 0e9bd69947482663a2fdc3031d0696b992a6e194.

--- Registration/dbo.logindata.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[logindata] (
-    [FirstName]    VARCHAR (50) NULL,
-    [LastName] VARCHAR (50) NULL, 
-    [Phoneno] NCHAR(10) NULL, 
-    [Email] VARCHAR(50) NULL, 
-    [Password] VARCHAR(50) NULL, 
-    [ConformPassword] VARCHAR(50) NULL
-);
-
