256f2633f0d1817310d18448884eb52bdf57c909
Jaswanth Sai <jaswanthsai266@gmail.com>
2024-10-10 19:45:43 +0530

    login & registration

---
+++ source/repos/WebApplication19/WebApplication19/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WebApplication19.Models
+{
+    public class RegisterModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string ApartmentName { get; set; }
+        public string Flatno { get; set; }
+        public string Pincode { get; set; }
+    }
+}
+
