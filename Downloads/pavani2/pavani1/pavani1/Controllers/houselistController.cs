04269e81990caf33b13a7ef782c40520597a6484
Jaswanthsai1672 <challajaswantsai786@gmail.com>
2024-10-11 21:21:34 +0530

    Combined files

---
+++ Downloads/pavani2/pavani1/pavani1/Controllers/houselistController.cs
@@ -0,0 +1,22 @@
+﻿using pavani1.Models;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace pavani1.Controllers
+{
+    public class HousesController : Controller
+    {
+        private HouseContext db = new HouseContext();
+
+        // GET: Houses
+        public ActionResult Index()
+        {
+            var houses = db.Houselists.ToList(); // Fetch data from database
+            return View(houses);
+        }
+    }
+}
+
+
+
---
+++ Downloads/pavani2/pavani1/pavani1/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace pavani1.Models
+{
+    public class HouseContext : DbContext
+    {
+        public DbSet<houselist> Houselists { get; set; }
+
+        public HouseContext() : base("name=HouseConnectionString")
+        {
+        }
+    }
+}
