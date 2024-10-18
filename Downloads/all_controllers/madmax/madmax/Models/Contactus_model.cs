5ac9af42b982280b745b63baa761f48f0db0ef13
Jaswanthsai1672 <challajaswantsai786@gmail.com>
2024-10-18 21:06:40 +0530

    all controllers

---
+++ Downloads/all_controllers/madmax/madmax/Models/Contactus_model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace madmax.Models
+{
+    public class Contactus_model
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
